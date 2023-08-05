69f3b5cf0b034f0b53836af6f8cae38bfc972eab
Mizunen <kon10419@hotmail.com>
2023-08-05 13:53:59 -0500

    Add Hashrouter and react-outer-dom

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import ReactDOM from "react-dom/client";
-
+import { HashRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import { CartContextProvider } from "./components/context/show-cart-context";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <CartContextProvider>
-    <App />
-  </CartContextProvider>
+  <HashRouter>
+    <CartContextProvider>
+      <App />
+    </CartContextProvider>
+  </HashRouter>
 );
